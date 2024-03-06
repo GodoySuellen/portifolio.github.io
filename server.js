41a742d7f45bd0c81088ee104d07088795391458
Suellen Godoy <36516387+GodoySuellen@users.noreply.github.com>
2024-03-06 18:37:28 -0300

    Update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,10 +7,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Servir os arquivos estáticos
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./")));
 app.use("/src", express.static(path.join(__dirname, "src")));
-app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
-app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules"))); // Corrigido para usar "/node_modules" em vez de "./node_modules"
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist'))); // Corrigido para usar '/' antes de 'bootstrap' para indicar que é um caminho absoluto
 
 // Configurar a porta
 const PORT = process.env.PORT || 80;
